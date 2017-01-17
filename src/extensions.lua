691c833ca6939befd65bfc23266db12670d172dc
Matteo Autieri <matteoautieri@live.it>
2017-01-17 14:13:20 +0100

    added more time scales

--- src/extensions.lua
+++ src/extensions.lua
@@ -41,3 +41,61 @@ InputBinding.getKeyNamesOfDigitalAction = function (actionIndex)
 		return "";
 	end
 end
+
+ function Utils.getTimeScaleIndex(timeScale)
+    if 15360 <= timeScale then
+		return 13;
+	elseif 7680 <= timeScale then
+		return 12;
+    elseif 3840 <= timeScale then
+		return 11;
+    elseif 1920 <= timeScale then
+		return 10;
+    elseif 960 <= timeScale then
+		return 9;
+    elseif 480 <= timeScale then
+		return 8;
+    elseif 240 <= timeScale then
+		return 7;
+    elseif 120 <= timeScale then
+		return 6;
+	elseif 60 <= timeScale then
+		return 5;
+	elseif 30 <= timeScale then
+		return 4;
+	elseif 15 <= timeScale then
+		return 3;
+	elseif 5 <= timeScale then
+		return 2;
+	end
+	return 1;
+end
+
+function Utils.getTimeScaleFromIndex(timeScaleIndex)
+    if 13 <= timeScaleIndex then
+		return 15360;
+	elseif 12 <= timeScaleIndex then
+		return 7680;
+    elseif 11 <= timeScaleIndex then
+		return 3840;
+    elseif 10 <= timeScaleIndex then
+		return 1920;
+    elseif 9 <= timeScaleIndex then
+		return 960;
+    elseif 8 <= timeScaleIndex then
+		return 480;
+    elseif 7 <= timeScaleIndex then
+		return 240;
+    elseif 6<= timeScaleIndex then
+		return 120;
+	elseif 5 <= timeScaleIndex then
+		return 60;
+	elseif 4 <= timeScaleIndex then
+		return 30;
+	elseif 3 <= timeScaleIndex then
+		return 15;
+	elseif 2 <= timeScaleIndex then
+		return 5;
+	end
+	return 1;
+end
