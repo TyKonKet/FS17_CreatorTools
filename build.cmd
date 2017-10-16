eebce2f344288047859dc7cbc65260a9d09ff90c
TyKonKet <matteoautieri@live.it>
2017-10-16 14:40:46 +0200

    new build system

--- build.cmd
+++ build.cmd
@@ -1,26 +1,42 @@
-@ECHO off
+@ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET my_path=%~dp0
 SET mod_path=%1
 SET mod_path=%mod_path:"=%
-for %%f in (%mod_path%) do SET mod_name=%%~nxf
-ECHO ############################## Starting build of %mod_name% ##############################
+FOR %%f IN (%mod_path%) DO SET mod_name=%%~nxf
 SET mod_path_src=%mod_path%\src
 SET mod_path_out=%mod_path%\out
 SET fs17_mods_path=%USERPROFILE%\Documents\My Games\FarmingSimulator2017\mods\
 SET fs17_mod_path=%fs17_mods_path%%mod_name%
-RMDIR /S /Q "%fs17_mod_path%\"
-DEL "%fs17_mods_path%%mod_name%.zip"
-::XCOPY "%mod_path_src%" "%fs17_mod_path%\" /S
-MKDIR "%fs17_mod_path%"
-PUSHD "%mod_path_src%"
+
+ECHO #################################### Starting build of %mod_name% ####################################
+
+IF EXIST "%fs17_mod_path%\" (
+    RMDIR /S /Q "%fs17_mod_path%\"
+    ECHO RMDIR %fs17_mod_path%\
+)
+IF EXIST "%fs17_mods_path%%mod_name%.zip" (
+     DEL "%fs17_mods_path%%mod_name%.zip"
+     ECHO DEL %fs17_mods_path%%mod_name%.zip
+)
+PUSHD %mod_path_src%
+IF NOT EXIST "%TEMP%\%mod_name%\" (
+    MKDIR "%TEMP%\%mod_name%\"
+)
+XCOPY /E /V /I /Q "%mod_path_src%" "%fs17_mod_path%"
+
+ECHO #################################### Starting debug of %mod_name% ####################################
+
 SET /a count = 0
-for /r %%a in (*) do (
-    COPY "%%a" "%fs17_mod_path%\%%~nxa" > nul
+FOR /R %%I IN (*.lua) DO (
+	call "C:\Program Files\luapower\luajit" -bg "%%~dpnI.lua" "%TEMP%\%mod_name%\%%~nI.luc"
     SET /a count += 1
 )
-echo Copied !count! file(s)
+ECHO %count% File debugged
 POPD
+
 ECHO #################################### %mod_name% built ####################################
+
 ENDLOCAL
+CALL "C:\Program Files (x86)\Farming Simulator 2017\FarmingSimulator2017.exe" -cheats -restart
 EXIT
