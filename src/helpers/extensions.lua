4b340059020ac73390d9e43e07cdb593e5e988a9
TyKonKet <matteoautieri@live.it>
2018-04-13 18:38:21 +0200

    new format style

--- src/helpers/extensions.lua
+++ src/helpers/extensions.lua
@@ -3,129 +3,129 @@
 --
 -- @author TyKonKet
 -- @date 11/01/2017
-CreatorToolsExtensions = {};
+CreatorToolsExtensions = {}
 
 function CreatorToolsExtensions:load()
-    g_currentMission.setAllowsHudDisplay = CreatorToolsExtensions.setAllowsHudDisplay;
+    g_currentMission.setAllowsHudDisplay = CreatorToolsExtensions.setAllowsHudDisplay
     if g_currentMission.backup == nil then
-        g_currentMission.backup = {};
+        g_currentMission.backup = {}
     end
 end
 
 function CreatorToolsExtensions:setAllowsHudDisplay(v)
-    g_currentMission.showHudEnv = v;
-    g_currentMission.showWeatherForecast = v;
-    g_currentMission.renderTime = v;
-    g_currentMission.showVehicleInfo = v;
+    g_currentMission.showHudEnv = v
+    g_currentMission.showWeatherForecast = v
+    g_currentMission.renderTime = v
+    g_currentMission.showVehicleInfo = v
     if v then
-        g_gameSettings:setValue("showHelpMenu", CreatorTools.backup.showHelpBox);
+        g_gameSettings:setValue("showHelpMenu", CreatorTools.backup.showHelpBox)
     else
-        CreatorTools.backup.showHelpBox = g_gameSettings:getValue("showHelpMenu");
-        g_gameSettings:setValue("showHelpMenu", false);
+        CreatorTools.backup.showHelpBox = g_gameSettings:getValue("showHelpMenu")
+        g_gameSettings:setValue("showHelpMenu", false)
     end
-    g_currentMission.showHudMissionBase = v;
-    g_currentMission.showHudMissionBaseOriginal = v;
-    g_currentMission.showVehicleSchema = v;
+    g_currentMission.showHudMissionBase = v
+    g_currentMission.showHudMissionBaseOriginal = v
+    g_currentMission.showVehicleSchema = v
 end
 
 -- real extension methods
 function Utils.getTimeScaleIndex(timeScale)
     if 15360 <= timeScale then
-        return 13;
+        return 13
     elseif 7680 <= timeScale then
-        return 12;
+        return 12
     elseif 3840 <= timeScale then
-        return 11;
+        return 11
     elseif 1920 <= timeScale then
-        return 10;
+        return 10
     elseif 960 <= timeScale then
-        return 9;
+        return 9
     elseif 480 <= timeScale then
-        return 8;
+        return 8
     elseif 240 <= timeScale then
-        return 7;
+        return 7
     elseif 120 <= timeScale then
-        return 6;
+        return 6
     elseif 60 <= timeScale then
-        return 5;
+        return 5
     elseif 30 <= timeScale then
-        return 4;
+        return 4
     elseif 15 <= timeScale then
-        return 3;
+        return 3
     elseif 5 <= timeScale then
-        return 2;
+        return 2
     end
-    return 1;
+    return 1
 end
 
 function Utils.getTimeScaleFromIndex(timeScaleIndex)
     if 13 <= timeScaleIndex then
-        return 15360;
+        return 15360
     elseif 12 <= timeScaleIndex then
-        return 7680;
+        return 7680
     elseif 11 <= timeScaleIndex then
-        return 3840;
+        return 3840
     elseif 10 <= timeScaleIndex then
-        return 1920;
+        return 1920
     elseif 9 <= timeScaleIndex then
-        return 960;
+        return 960
     elseif 8 <= timeScaleIndex then
-        return 480;
+        return 480
     elseif 7 <= timeScaleIndex then
-        return 240;
+        return 240
     elseif 6 <= timeScaleIndex then
-        return 120;
+        return 120
     elseif 5 <= timeScaleIndex then
-        return 60;
+        return 60
     elseif 4 <= timeScaleIndex then
-        return 30;
+        return 30
     elseif 3 <= timeScaleIndex then
-        return 15;
+        return 15
     elseif 2 <= timeScaleIndex then
-        return 5;
+        return 5
     end
-    return 1;
+    return 1
 end
 
 function Utils.getNumTimeScales()
-    return 13;
+    return 13
 end
 
 function Utils.getTimeScaleString(timeScaleIndex)
     if timeScaleIndex == 1 then
-        return g_i18n:getText("ui_realTime");
+        return g_i18n:getText("ui_realTime")
     else
-        return string.format("%dx", Utils.getTimeScaleFromIndex(timeScaleIndex));
+        return string.format("%dx", Utils.getTimeScaleFromIndex(timeScaleIndex))
     end
 end
 
 function InputBinding.getKeysNamesOfDigitalAction(actionIndex)
-    local actionData = InputBinding.actions[actionIndex];
-    local k1, k2, m1 = nil;
+    local actionData = InputBinding.actions[actionIndex]
+    local k1, k2, m1 = nil
     if #actionData.keys1 > 0 then
-        k1 = InputBinding.getKeyNames(actionData.keys1);
+        k1 = InputBinding.getKeyNames(actionData.keys1)
     end
     if #actionData.keys2 > 0 then
-        k2 = InputBinding.getKeyNames(actionData.keys2);
+        k2 = InputBinding.getKeyNames(actionData.keys2)
     end
     if #actionData.mouseButtons > 0 then
-        m1 = InputBinding.getMouseButtonNames(actionData.mouseButtons);
+        m1 = InputBinding.getMouseButtonNames(actionData.mouseButtons)
     end
     if k1 ~= nil and k2 ~= nil then
-        return string.format("%s %s %s", k1, g_i18n:getText("ui_and"), k2);
+        return string.format("%s %s %s", k1, g_i18n:getText("ui_and"), k2)
     end
     if k1 ~= nil then
-        return k1;
+        return k1
     end
     if k2 ~= nil then
-        return k2;
+        return k2
     end
     if m1 ~= nil then
         return m1
     end
-    return "";
+    return ""
 end
 
 function InputBinding.getMouseButtonNames(mouseButtons)
-    return g_i18n:getText("ui_mouse") .. " " .. MouseHelper.getButtonNames(mouseButtons);
+    return g_i18n:getText("ui_mouse") .. " " .. MouseHelper.getButtonNames(mouseButtons)
 end
--- src/helpers/helpLineLoader.lua
+++ src/helpers/helpLineLoader.lua
@@ -4,53 +4,56 @@
 -- @author TyKonKet
 -- @date 14/02/2017
 function loadHelpLine(xml, helpLineCategories, helpLineCategorySelectorElement, modDirectory)
-    xml = loadXMLFile("customHelpLineViewContentXML", xml);
-    local categoriesIndex = 0;
+    xml = loadXMLFile("customHelpLineViewContentXML", xml)
+    local categoriesIndex = 0
     while true do
-        local categoryQuery = string.format("helpLines.helpLineCategory(%d)", categoriesIndex);
+        local categoryQuery = string.format("helpLines.helpLineCategory(%d)", categoriesIndex)
         if not hasXMLProperty(xml, categoryQuery) then
-            break;
+            break
         end
         local category = {
             title = getXMLString(xml, string.format("%s#title", categoryQuery)),
             helpLines = {}
-        };
-        helpLineCategorySelectorElement:addText(g_i18n:getText(category.title));
-        local helpLinesIndex = 0;
+        }
+        helpLineCategorySelectorElement:addText(g_i18n:getText(category.title))
+        local helpLinesIndex = 0
         while true do
-            local helpLineQuery = string.format("%s.helpLine(%d)", categoryQuery, helpLinesIndex);
+            local helpLineQuery = string.format("%s.helpLine(%d)", categoryQuery, helpLinesIndex)
             if not hasXMLProperty(xml, helpLineQuery) then
-                break;
+                break
             end
             local helpLine = {
                 title = getXMLString(xml, string.format("%s#title", helpLineQuery)),
                 items = {}
-            };
-            local itemsIndex = 0;
+            }
+            local itemsIndex = 0
             while true do
-                local itemQuery = string.format("%s.item(%d)", helpLineQuery, itemsIndex);
+                local itemQuery = string.format("%s.item(%d)", helpLineQuery, itemsIndex)
                 if not hasXMLProperty(xml, itemQuery) then
-                    break;
+                    break
                 end
-                local itemType = getXMLString(xml, string.format("%s#type", itemQuery));
-                local itemValue = getXMLString(xml, string.format("%s#value", itemQuery));
+                local itemType = getXMLString(xml, string.format("%s#type", itemQuery))
+                local itemValue = getXMLString(xml, string.format("%s#value", itemQuery))
                 if itemType == "image" then
-                    itemValue = modDirectory .. itemValue;
+                    itemValue = modDirectory .. itemValue
                 end
                 if (itemType == "text" or itemType == "image") and itemValue ~= nil then
-                    table.insert(helpLine.items, {
-                        type = itemType,
-                        value = itemValue,
-                        heightScale = Utils.getNoNil(getXMLFloat(xml, string.format("%s#heightScale", itemQuery)), 1)
-                    });
+                    table.insert(
+                        helpLine.items,
+                        {
+                            type = itemType,
+                            value = itemValue,
+                            heightScale = Utils.getNoNil(getXMLFloat(xml, string.format("%s#heightScale", itemQuery)), 1)
+                        }
+                    )
                 end
-                itemsIndex = itemsIndex + 1;
+                itemsIndex = itemsIndex + 1
             end
-            table.insert(category.helpLines, helpLine);
-            helpLinesIndex = helpLinesIndex + 1;
+            table.insert(category.helpLines, helpLine)
+            helpLinesIndex = helpLinesIndex + 1
         end
-        table.insert(helpLineCategories, category);
-        categoriesIndex = categoriesIndex + 1;
+        table.insert(helpLineCategories, category)
+        categoriesIndex = categoriesIndex + 1
     end
-    delete(xml);
+    delete(xml)
 end
